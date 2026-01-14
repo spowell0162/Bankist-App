cbfc0aff3fcb342f1788d886e4288899cb0c0fa2
samuelpowellweb <samuelpowellweb@gmail.com>
2026-01-14 10:18:36 +0000

    coding challeng #1

--- script.js
+++ script.js
@@ -141,3 +141,18 @@ displayMovements(account1.movements)
 // })
 
 /////////////////////////////////////////////////
+
+// Challenge #1
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  let dogsOnlyJulia = dogsJulia.slice(1, -2);
+  const allDogs = dogsOnlyJulia.concat(dogsKate);
+  allDogs.forEach(function (dogAge, index) {
+    if (dogAge >= 3) {
+      console.log(`Dog number ${index + 1} is an adult, and is ${dogAge} years old`);
+    } else console.log(`Dog number ${index + 1} is still a puppy`);
+  })
+}
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]); 
