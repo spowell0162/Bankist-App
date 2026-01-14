a22500efb85bd30791ccc9ec979577d258f4830f
samuelpowellweb <samuelpowellweb@gmail.com>
2026-01-14 10:59:25 +0000

    map method

--- script.js
+++ script.js
@@ -78,6 +78,7 @@ displayMovements(account1.movements)
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+
 // LECTURES
 
 // const currencies = new Map([
@@ -144,15 +145,29 @@ displayMovements(account1.movements)
 
 // Challenge #1
 
-const checkDogs = function (dogsJulia, dogsKate) {
-  let dogsOnlyJulia = dogsJulia.slice(1, -2);
-  const allDogs = dogsOnlyJulia.concat(dogsKate);
-  allDogs.forEach(function (dogAge, index) {
-    if (dogAge >= 3) {
-      console.log(`Dog number ${index + 1} is an adult, and is ${dogAge} years old`);
-    } else console.log(`Dog number ${index + 1} is still a puppy`);
-  })
-}
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   let dogsOnlyJulia = dogsJulia.slice(1, -2);
+//   const allDogs = dogsOnlyJulia.concat(dogsKate);
+//   allDogs.forEach(function (dogAge, index) {
+//     if (dogAge >= 3) {
+//       console.log(`Dog number ${index + 1} is an adult, and is ${dogAge} years old`);
+//     } else console.log(`Dog number ${index + 1} is still a puppy`);
+//   })
+// }
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]); 
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// map Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movementsUSD);
+
+const movementsDescriptions = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? `deposited` : `withdrew`} ${Math.abs(mov)}`);
+
+console.log(movementsDescriptions);
