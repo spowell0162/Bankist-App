72826855f33014aa6a4f902c79f51d56b573ac8d
samuelpowellweb <samuelpowellweb@gmail.com>
2026-01-16 10:11:56 +0000

    computing usernames

--- script.js
+++ script.js
@@ -74,7 +74,20 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements)
+displayMovements(account1.movements);
+
+const createUsernames = function (accounts) {
+  accounts.forEach(function (account) {
+    account.username = account.owner.toLowerCase().split(' ').map(name => name[0]).join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
+
+
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -160,14 +173,14 @@ displayMovements(account1.movements)
 
 // map Method
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const eurToUsd = 1.1;
+// const eurToUsd = 1.1;
 
-const movementsUSD = movements.map(mov => mov * eurToUsd);
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
 
-console.log(movementsUSD);
+// console.log(movementsUSD);
 
-const movementsDescriptions = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? `deposited` : `withdrew`} ${Math.abs(mov)}`);
+// const movementsDescriptions = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? `deposited` : `withdrew`} ${Math.abs(mov)}`);
 
-console.log(movementsDescriptions);
+// console.log(movementsDescriptions);
