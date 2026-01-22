414e2740f8841b465b3c2ead71b23e16bcf931d7
samuelpowellweb <samuelpowellweb@gmail.com>
2026-01-22 12:46:03 +0000

    challenge #3

--- script.js
+++ script.js
@@ -249,3 +249,16 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //   .reduce((acc, mov) => acc + mov, 0);
 
 // console.log(totalDepositsUSD);
+
+// Challenge 3
+
+const calcAverageHumanAge = function (ageArray) {
+  const humanAge = ageArray
+    .map(age => age <= 2 ? age * 2 : 16 + age * 4)
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  return humanAge;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
