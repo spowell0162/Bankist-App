45d773c9cec2edd630572d7e59e2c71bdeaf683b
samuelpowellweb <samuelpowellweb@gmail.com>
2026-01-26 11:59:39 +0000

    the findIndex method

--- script.js
+++ script.js
@@ -147,7 +147,19 @@ btnTransfer.addEventListener('click', function (e) {
     receiverAcc.movements.push(amount);
     updateUI(currentAccount);
   }
-})
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+    // delete account
+    accounts.splice(index, 1);
+    // hide ui
+    containerApp.style.opacity = 0;
+  };
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
